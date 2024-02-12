2443e548d61fca66e1fa20e51f457e1095fa51b1
Abyravka90 <asepcahya.nugraha75@gmail.com>
2024-02-12 17:15:36 +0700

    foreign-key relation database

--- resources/views/public/post/detail.blade.php
+++ resources/views/public/post/detail.blade.php
@@ -1,5 +1,18 @@
 @extends('layouts.frontend')
 
 @section('content')
-    <h2>Hello World</h2>
+<div class="container py-5">
+    <div class="row">
+        <div class="col-md-6 mx-auto text-center">
+            <h1><p>Title</p></h1>
+            <p class="text-muted">12 Februari 2024 Created By Asep Cahya Nugraha <a href="">Laravel</a></p>
+        </div>
+        <div class="col-md-8 mx-auto d-flex justify-content-center">
+            <div class="w-100" style="background-color:grey;height:500px;border-radius:20px;"></div>
+        </div>
+        <div class="col-md-8 mx-auto mt-4">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatem excepturi earum laborum itaque commodi suscipit eaque dolorem aliquam debitis ipsa iure, placeat quo voluptas quibusdam est! Ipsum, nostrum. Perspiciatis, reprehenderit.
+        </div>
+    </div>
+</div>
 @endsection
