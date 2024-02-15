2537d9f6dd0354a48d914eb47aa3e65efc4cfc05
Abyravka90 <asepcahya.nugraha75@gmail.com>
2024-02-15 14:43:54 +0700

    validation, Mass Assignment

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -17,13 +17,31 @@ class PostController extends Controller
     {
         // dd($request);
         // $request->dd();
-        $post = new Post();
-        $post->user_id = auth()->user()->id;
-        $post->category_id = $request->category_id;
-        $post->title = $request->title;
-        $post->slug = \Str::slug($request->title);
-        $post->image = 'default-image';
-        $post->description = $request->description;
-        $post->save();
+        // $post = new Post();
+        // $post->user_id = auth()->user()->id;
+        // $post->category_id = $request->category_id;
+        // $post->title = $request->title;
+        // $post->slug = \Str::slug($request->title);
+        // $post->image = 'default-image';
+        // $post->description = $request->description;
+        // $post->save();
+        $this->validate($request, [
+            'title' => 'required',
+            // 'image' => 'required',
+            'description' => 'required'
+        ], [
+            'title.required' => 'Judul Harus Diisi',
+            // 'image.required' => 'Gambar Harus Diisi',
+            'description.required' => 'Deskripsi Harus Diisi',
+        ]);
+
+        Post::create([
+            'title' => $request->title,
+            'user_id' => auth()->user()->id,
+            'category_id' => $request->category_id,
+            'slug' => \Str::slug($request->title),
+            'image' => 'default-image',
+            'description' => $request->description,
+        ]);
     }
 }
